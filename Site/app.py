5afe8ca62b90cd711cc189d07ec5dd7cc3594365
alangaubert <alan.gaubert22@gmail>
2025-06-13 13:31:45 +0200

    Update Flask

--- Site/app.py
+++ Site/app.py
@@ -2,24 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/index_fr')
-def index_fr()
+
+@app.route('/')
+def index_fr():
     return render_template('index_fr.html')
 
 @app.route('/batterie_fr')
-def batterie_fr()
+def batterie_fr():
     return render_template('batterie_fr.html')
 
 @app.route('/ecouteurs_fr')
-def ecouteurs_fr()
+def ecouteurs_fr():
     return render_template('ecouteurs_fr.html')
 
 @app.route('/panier_fr')
-def panier_fr()
+def panier_fr():
     return render_template('panier_fr.html')
 
 @app.route('/about_fr')
-def about_fr()
+def about_fr():
     return render_template('about_fr.html')
 
 @app.route('/index_en')
